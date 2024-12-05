bfb6b8265aa6bf92d04d366afd07a5c2bfe810c0
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-06 01:47:48 +0530

    if I don't add `return res.end();` inside if statement of `/favicon.ico route` then app crashed with a error -> (Error [ERR_STREAM_WRITE_AFTER_END]: write after end)

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const http = require('http');
 //    -> keep in mind the server isn't running right now, .createServer() creates a server and give it to you that's it.
 const server = http.createServer(( req, res)=>{
   if (req.url==='/favicon.ico') {
-    console.log("Inside /favicon.ico route path: "+req.url);
     res.writeHead(204); // No Content
-    return res.end();
+    res.end();
+    console.log("inside /fabicon.ico and after res.end()");
   }
   console.log("path: "+req.url);
   res.end("Hello World! - Function Ends.");
