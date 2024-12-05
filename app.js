1a1c73bc1e52a4bc5e4ec1c62d62af2fff38fe45
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-06 01:33:31 +0530

    restricting 2time console.log from createServer()

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const http = require('http');
 // .createServer() is a method in http module which returns -> instance of a server.
 //    -> keep in mind the server isn't running right now, .createServer() creates a server and give it to you that's it.
 const server = http.createServer(( req, res)=>{
-  console.log("req.url: "+req.url)
-  console.log("Successfully created the server.");
+  if (req.url==='/favicon.ico') {
+    console.log("Inside /favicon.ico route path: "+req.url);
+    res.writeHead(204); // No Content
+    return res.end();
+  }
+  console.log("path: "+req.url);
   res.end("Hello World! - Function Ends.");
 }); // -> here we created the server
 
