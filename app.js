c9b8bfa5df40fc8537396793655c77e74bc4b503
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-06 01:12:42 +0530

    when I hit localhost:3000 then why 2time Successfully created the server. message is showing though I have wretten this line 1time in the code

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const http = require('http');
 // .createServer() is a method in http module which returns -> instance of a server.
 //    -> keep in mind the server isn't running right now, .createServer() creates a server and give it to you that's it.
 const server = http.createServer(( req, res)=>{
+  console.log("req.url: "+req.url)
   console.log("Successfully created the server.");
-  res.end("Hello World!");
+  res.end("Hello World! - Function Ends.");
 }); // -> here we created the server
 
 // .listen() method run the created server.
