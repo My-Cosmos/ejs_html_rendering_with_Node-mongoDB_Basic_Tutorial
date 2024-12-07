a53b6ae786bb839b1b1bc6a9c98c4beab3d8ea45
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-07 11:23:18 +0530

    Instead of plain Node.js using express to create routes and running the server

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const http = require('http');
+// ~ starting using express to create server and routes instead of plain Node.js previously.
 
+//const http = require('http');
+
+const express = require('express');
+const app = express();
+
+
+/*
 const server = http.createServer(( req, res )=>{
   if(req.url=='/about'){
     res.end("This is About page");
@@ -16,5 +23,29 @@ const server = http.createServer(( req, res )=>{
     res.end("Home page");
   }
 });
+*/
 
-server.listen(3000);
+// ~ creating route using app.get();
+app.get('/', (req,res)=>{
+  // ~ instead of res.end(); here we send res.send() from this app.get() routes ~ This is the best practice
+  res.send('This is Home page');
+})
+
+app.get('/about', (req,res)=>{
+  res.send('This is About page');
+})
+
+app.get('/post', (req,res)=>{
+  res.send('This is Post page');
+})
+
+app.get('/login', (req,res)=>{
+  res.send('This is Login page');
+})
+
+app.get('/signup', (req,res)=>{
+  res.send('This is Signup page');
+})
+
+// ~ Remember here in express.js we no need to create server (.createServer()), only if we call app.listen() then only express create the server internally and run/listen to the server.
+app.listen(3000);
