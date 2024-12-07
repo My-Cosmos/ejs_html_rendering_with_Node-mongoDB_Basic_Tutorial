eb6c03bdef84e2832f56b498ad4b409f05029963
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-07 12:12:37 +0530

    This is how we can render html using ejs_view_engine and node_express_app

--- Note.md
+++ Note.md
@@ -1,3 +1,7 @@
+~ To render html using backend(express backend) we need to setup **view Engine**
+~~ There are many view engine available there, those are pub,ejs e.t.c
+
+
 Server e kono Request ele [ like localhost:3000/, localhost:3000/about, localhost/post, /contactus e.t.c ]
 ei function ta chole -> .createServer((req,res)=>{})
 ```
--- app.js
+++ app.js
@@ -1,51 +1,13 @@
-// ~ starting using express to create server and routes instead of plain Node.js previously.
-
-//const http = require('http');
-
 const express = require('express');
 const app = express();
 
+// ~ after installing ejs we no need to require ejs, 
+  // ~~ we can directly use/set ejs using app.set().
+app.set("view engine", "ejs");
 
-/*
-const server = http.createServer(( req, res )=>{
-  if(req.url=='/about'){
-    res.end("This is About page");
-  }
-  else if(req.url=='/post'){
-    res.end("This is Post page");
-  }
-  else if(req.url=='/login'){
-    res.end("This is Login page");
-  }
-  else if(req.url=='/signup'){
-    res.end("This is Signup page");
-  }else{
-    res.end("Home page");
-  }
-});
-*/
-
-// ~ creating route using app.get();
 app.get('/', (req,res)=>{
-  // ~ instead of res.end(); here we send res.send() from this app.get() routes ~ This is the best practice
-  res.send('This is Home page');
+  res.render('home'); //This will go to 'views/' folder and render the "/home.ejs" file.
 })
 
-app.get('/about', (req,res)=>{
-  res.send('This is About page');
-})
 
-app.get('/post', (req,res)=>{
-  res.send('This is Post page');
-})
-
-app.get('/login', (req,res)=>{
-  res.send('This is Login page');
-})
-
-app.get('/signup', (req,res)=>{
-  res.send('This is Signup page');
-})
-
-// ~ Remember here in express.js we no need to create server (.createServer()), only if we call app.listen() then only express create the server internally and run/listen to the server.
 app.listen(3000);
