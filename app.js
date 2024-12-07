aad35c1b28e41bbf37f528b486ac4f0e6d7489af
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-07 14:36:11 +0530

    Handled first form but here empty object will come in eq.query because we have to give name of the data inside input fields that is going to receive in the server

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get('/', (req,res,next)=>{
-    console.log("This is middleware");
-    return next();
-  },(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('home');
   })
-app.get('/post', 
-  (req,res,next)=>{return next();},
-  (req,res)=>{
-    res.send('This is post page');
-  }
-)
+app.get('/login',(req,res)=>{
+    res.render('login');
+  })
+
+
+// ~ For receiving form data we need to create a route
+app.get('/get-form-data', (req,res)=>{
+  console.log(req.query);
+  res.send("Data Received");
+})
+
+
+
 
 app.listen(3000);
