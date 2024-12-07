0b9869dc3c51ab79cb6e3e3e1d0f3feca6e27fcb
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-07 13:52:24 +0530

    Created custom middlewares for specific routes - whihch will only run if the request comes to the specific routes

--- app.js
+++ app.js
@@ -3,25 +3,17 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-// ~ This is how we can use middleware
-  // ~ ar ekhaney kono specific route nei bole server e jekono request elei[example:- '/', '/post', '/about'] ei middleware er moddhe dhukbey tarpor next(); function ta pele respective ['/', '/post', '/profile', '/about', '/contact'] route e chole jabe
-app.use((req,res,next)=>{
-  console.log("This is middleware");
-
-  const a=2;
-  const b=3;
-
-  console.log(a+b);
-
-  return next();
-})
-
-app.get('/', (req,res)=>{
-  res.render('home');
-})
-app.get('/post', (req,res)=>{
-  res.send('This is post page');
-})
-
+app.get('/', (req,res,next)=>{
+    console.log("This is middleware");
+    return next();
+  },(req,res)=>{
+    res.render('home');
+  })
+app.get('/post', 
+  (req,res,next)=>{return next();},
+  (req,res)=>{
+    res.send('This is post page');
+  }
+)
 
 app.listen(3000);
